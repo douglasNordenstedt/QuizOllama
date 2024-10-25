0bc90246c07013ae8e68f96a63301965ac42eb00
Douglas Nordenstedt <douglas.nordenstedt@eciceron.com>
2024-10-25 15:59:48 +0200

    updated footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="left-0 w-full bg-myTwo text-myFour p-4 flex justify-center items-center">
-        <p>Footer</p>
-        <p>footer</p>
+  <div class="left-0 w-full bg-myTwo text-myFour p-4 flex justify-center items-center flex-col italic">
+    <div>
+      <a href="https://github.com/douglasNordenstedt/QuizOllama" target="_blank">Source Code</a>
+    </div>
+    <div class ="flex flex-row">
+      <a href="https://uiverse.io/BlackisPlay/pretty-sloth-9" target="_blank">Loading icon</a>
+      <a class="pl-5 pr-5" href="https://github.com/ollama/ollama-js" target="_blank">Ollama.js</a>
+    </div>
+        
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,6 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
   ],
   devServer: {
-    host: '0.0.0.0'
+    host: '127.0.0.1'
   },
 })
